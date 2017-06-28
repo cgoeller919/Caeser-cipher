a6346392dc9c70767eb4c7deeaa76db6efdf0ed6
cgoeller919 <guitarguy919@gmail.com>
2017-06-28 06:05:58 -0400

    Caeser Cipher v0.1.2
    Added notes to thoroughly explain script

--- caeser cipher.py
+++ caeser cipher.py
@@ -1,43 +1,41 @@
 #Caeser Cipher Encryption/Decryption
 
-MAX_KEY_SIZE = 26
-
-def getKey():
+def getKey(): #gets key size from user
     key = int(input('Enter the key value:'))
-    if key > MAX_KEY_SIZE:
+    if key > 26: #ensures key is not bigger than the amount of characters in the alphabet
         print('Key size too big. Please try again.')
         getKey()
-    return key
+    return key #returns key as variable when function is called
 
-def getPhrase():
-    phrase = input('Enter the cipher text:')
-    return phrase
+def getPhrase(): #gets plain text or cipher text from user
+    phrase = input('Enter the cipher text or plain text:')
+    return phrase #returns text as string when function is called
 
-def encrypt():
-    key = getKey()
-    phrase = getPhrase()
-    cipher = ""
-    for cipherText in phrase:
-        cipherVal = ord(cipherText) + key
-        if cipherVal > ord('z'):
+def encrypt(): #encryption function
+    key = getKey() #assigns key variable from getKey() function
+    phrase = getPhrase() #assigns phrase string from getPhrase() function
+    cipher = "" #initializes cipher string
+    for cipherText in phrase: #loops for each character in 'phrase'
+        cipherVal = ord(cipherText) + key #converts each character into a numeric value and shifts it up based on the key
+        if cipherVal > ord('z'): #ensures values reset if they are greater than the numeric value of z
             cipherVal -= 26
-        cipherChar = chr(cipherVal)
-        cipher += cipherChar
+        cipherChar = chr(cipherVal) #converts the encrypted numeric values back to their now encrypted characters
+        cipher += cipherChar #creates a string based on the characters
     print(cipher)
 
-def decrypt():
-    key = getKey()
-    phrase = getPhrase()
-    cipher = ""
-    for cipherText in phrase:
-        cipherVal = ord(cipherText) - key
-        if cipherVal < ord('a'):
+def decrypt(): #decryption function
+    key = getKey() #assigns key variable from getKey() function
+    phrase = getPhrase() #assigns phrase string from getPhrase() function
+    cipher = "" #initializes cipher string
+    for cipherText in phrase: #loops for each character in 'phrase'
+        cipherVal = ord(cipherText) - key #converts each character into a numeric value and shifts it down based on the key
+        if cipherVal < ord('a'): #ensures values reset if they are less than the numeric value of a
             cipherVal += 26
-        cipherChar = chr(cipherVal)
-        cipher += cipherChar
+        cipherChar = chr(cipherVal) #converts the encrypted numeric values back to their now encrypted characters
+        cipher += cipherChar #creates a string based on the characters
     print(cipher)
 
-def caeser():
+def caeser(): #main function, collects whether to encrypt or decrypt a phrase from the user and runs the appropriate function
     command = input('Do you wish to (e)ncrypt or (d)ecrypt a cipher?')
     mode = 'A'
     if command == 'd':
